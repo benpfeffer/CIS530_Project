ba428ba375ace0acf64ed0af47c2a1f2bbef48ee
benpfeffer <55898447+benpfeffer@users.noreply.github.com>
2023-04-18 13:03:36 -0400

    Simplifying

--- Pizza_recommendation_plots.py
+++ Pizza_recommendation_plots.py
@@ -1,7 +1,6 @@
 
 
 import pandas as pd
-#import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
@@ -18,57 +17,16 @@ def plot_recs(cart):
 
     if(len(cart)>0):
         try:
-
-            od = pd.read_csv("pizza_sales/order_details.csv")
-            o = pd.read_csv("pizza_sales/orders.csv")
-            pt = pd.read_csv("pizza_sales/pizza_types.csv", encoding= 'unicode_escape') # codec decode error
-            p = pd.read_csv("pizza_sales/pizzas.csv")
-
-
-            ood = o.set_index('order_id').join(od.set_index('order_id')).reset_index()
-
-
-            ppt = p.set_index('pizza_type_id').join(pt.set_index('pizza_type_id')).reset_index()
-
-
-
-            df = ood.set_index('pizza_id').join(ppt.set_index('pizza_id')).reset_index().sort_values(by="order_details_id").reset_index(drop=True)
-
-
-
-            groups = df.groupby("order_id").agg({"pizza_type_id":"count"}).reset_index()
-            multiOrders = groups[groups.pizza_type_id>1].order_id.values
-
-
-            # multi-order dataframe
-            moDf = df[df.order_id.isin(multiOrders)].reset_index(drop=True)
-
-
-
-            simple = moDf[["order_id", "quantity", "pizza_type_id"]]
-            copyDf = simple.copy()
-            #copyDf = copyDf[copyDf.quantity==1]
-            copyDf['quantity'] = 1
-
-
-            copyDf = copyDf.drop_duplicates().reset_index(drop=True)
-
-
-            itemitem = copyDf.pivot(index='pizza_type_id', columns='order_id', values='quantity').fillna(0)
-
-
-            # Given a cart, average cosine sim for each item and display the optimal n (5)
-
-            # FINAL SYSTEM - USED LIVE AND INCLUDES ALL GIVEN DATA
-            map_pizza = itemitem.index.to_list()
-
-            knn = NearestNeighbors(metric='cosine', algorithm='brute')
-            knn.fit(itemitem.values)
-            distances, indices = knn.kneighbors(itemitem.values, n_neighbors=len(map_pizza)) #+1 for removal of self similarity
+            distances = np.loadtxt('distances.txt')
+            indices = np.loadtxt('indices.txt').astype(int)
+            # opening the file and extract pizza mapped to indices
+            mp = open("map.txt", "r")
+            mpd = mp.read()
+            map_pizza = mpd.split('\n')
+            mp.close()
 
 
             plot_data = []
-            #cart = ["bbq_ckn", "hawaiian", "five_cheese"]
             n = 5 # 5 pizza recommendations
             for i,item in enumerate(cart):
                 idx = map_pizza.index(item)
